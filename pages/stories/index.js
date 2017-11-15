c46fccb1a07d5a9551b0324b9d24479507865432
tianyuan <1246254102@qq.com>
2017-11-15 12:18:55 +0800

    wancheng

--- pages/stories/index.js
+++ pages/stories/index.js
@@ -1,17 +1,45 @@
+
+const app = getApp()
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    entities: null,
+    currentVid: null,
+    currentVideo: null
   },
+
+
+play(event){
+  
+  if (this.data.currentVid !== null) {
+    this.data.currentVideo.pause()
+  }
+  
+  
+  const currentVideo = wx.createVideoContent(
+    '${ event.target.dataset.vid}')
+  currentVideo.play()
+      
+  this.setData({
+    currentVideo,
+    currentVid:event.target.dataset.vid
+  })
+
+},
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.setData({
+      entities: app.globalData.stories
+    })
   },
 
   /**
